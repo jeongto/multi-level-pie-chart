6cdc3553dc3339daf438ce5c7849d5c19fc3e7fc
yoyota <yoyota@users.noreply.github.com>
2020-03-29 08:40:21 +0000

    deploy: 902cd2f9b23832b1b0df955c111a689253dc0e52

---
+++ precache-manifest.58f7e0485bc3270dd79cd5820ab16a9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8272ecea65866996ea",
+    "url": "build/2.2f8272ec.js"
+  },
+  {
+    "revision": "83dd3b9f936bae33673558d2b605daa2",
+    "url": "build/2.2f8272ec.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a4c5011a8a6d39ba32",
+    "url": "build/bundle.39a4c501.js"
+  },
+  {
+    "revision": "e7cece95a2a035a33cee",
+    "url": "build/main.e7cece95.js"
+  },
+  {
+    "revision": "ec87a43eeba057900591820c5362bd0c",
+    "url": "index.html"
+  }
+]);
